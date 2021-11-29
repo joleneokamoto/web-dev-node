0a7eae0882ade220b5c30cc94883d84598f7dcda
Jolene Okamoto <okamoto.j@northeastern.edu>
2021-11-29 18:28:44 -0500

    a9 fa2021

--- services/tweeter-service.js
+++ services/tweeter-service.js
@@ -1,61 +1,24 @@
-let tweets = require('../data/tweets.json');
+const dao = require('../tweets/tweet-dao');
 
 module.exports = (app) => {
+    const findAllTweets = (req, res) =>
+        dao.findAllTweets()
+            .then(tweets => res.json(tweets));
 
-    const findAllTweets = (req, res) => {
-        res.json(tweets);
-    };
-    app.get('/api/tweets', findAllTweets);
+    const createTweet = (req, res) =>
+        dao.createTweet(req.body)
+            .then((insertedTweet) => res.json(insertedTweet));
 
-    const postNewTweet = (req, res) => {
-        const newTweet = {
-            _id: (new Date()).getTime() + '',
-            "topic": "Web Development",
-            "userName": "ReactJS",
-            "verified": false,
-            "handle": "ReactJS",
-            "time": "2h",
-            "avatar-image": "../../../images/react-blue.png",
-            "logo-image": "../../../images/react-blue.png",
-            "stats": {
-                "comments": 123,
-                "retweets": 234,
-                "likes": 345
-            },
-            ...req.body,
-        };
-        tweets = [
-            newTweet,
-            ...tweets
-        ];
-        res.json(newTweet);
-    };
-    app.post('/api/tweets', postNewTweet);
+    const deleteTweet = (req, res) =>
+        dao.deleteTweet(req.params.id)
+            .then((status) => res.send(status));
 
-    const deleteTweet = (req, res) => {
-        const id = req.params['id'];
-        tweets = tweets.filter(tweet => tweet._id !== id);
-        res.sendStatus(200);
-    };
-    app.delete('/api/tweets/:id', deleteTweet);
+    const likeTweet = (req, res) =>
+        dao.updateTweet(req.params.id, req.body)
+            .then((status) => res.send(status));
 
-    const likeTweet = (req, res) => {
-        const id = req.params['id'];
-        tweets = tweets.map(tweet => {
-            if (tweet._id === id) {
-                if (tweet.liked === true) {
-                    tweet.liked = false;
-                    tweet.stats.likes--;
-                } else {
-                    tweet.liked = true;
-                    tweet.stats.likes++;
-                }
-                return tweet;
-            } else {
-                return tweet;
-            }
-        });
-        res.sendStatus(200);
-    };
     app.put('/api/tweets/:id/like', likeTweet);
+    app.delete('/api/tweets/:id', deleteTweet);
+    app.post('/api/tweets', createTweet);
+    app.get('/api/tweets', findAllTweets);
 };
